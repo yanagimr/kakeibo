12be1525d242907bb01fe34391713580286ba592
ファあ <k>
2026-01-17 16:17:03 +0900

    a

--- kakeibo/kakeiboApp.swift
+++ kakeibo/kakeiboApp.swift
@@ -18,20 +18,3 @@ struct kakeiboApp: App {
         }
     }
 }
-//
-//  kakeiboApp.swift
-//  kakeibo
-//
-//  Created by work on 2026/01/17.
-//
-
-import SwiftUI
-
-@main
-struct kakeiboApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
